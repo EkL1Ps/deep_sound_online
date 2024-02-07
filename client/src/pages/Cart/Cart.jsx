e7d85b90e0fdbe7616a8523a40fc152ab040c2ff
Ekuripusu <dogasshole.test@gmail.com>
2024-02-07 16:47:29 +0200

    created branch and

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 function Cart() {
   return (
-    <div style={{ fontSize: "40px", marginLeft: "100px" }}>
-      It`s your Cart
-    </div>
+    <div style={{ fontSize: "40px", marginLeft: "100px" }}>It`s your Cart</div>
   );
 }
 export default Cart;
