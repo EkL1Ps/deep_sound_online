46160dadc823c9f08505e27998422b271538c785
Ekuripusu <dogasshole.test@gmail.com>
2024-02-07 16:54:02 +0200

    code fix

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,37 +1,32 @@
 import React from "react";
 import "./Home.scss";
 import { NavLink } from "react-router-dom";
-<<<<<<< HEAD
+import arrowSubscribe from "../../images/footer/arrow.svg";
 import CardItem from "../../components/CardItem/CardItem.jsx";
 
 function Home() {
   return (
-    <div className="container">
-      <div>
-        <img
-          style={{ zIndex: "1" }}
-          src="/images/mainpage.png"
-          alt="mainpage"
-        />
-        <h2 className="img__title">Erzetich - a new preamp series</h2>
-        <h5 className="img__subtitle">
-          A fully new preamp station Erzitech - Bncillus
-        </h5>
-        <NavLink to="/about" className="img__btn">
-          <span style={{ display: "block", margin: "6px 10px 20px 60px" }}>
-            Learn more
-          </span>
-        </NavLink>
-      </div>
-
-      <CardItem />
-    </div>
-=======
-import arrowSubscribe from "../../images/footer/arrow.svg";
-
-function Home() {
-  return (
     <>
+      <div className="container">
+        <div>
+          <img
+            style={{ zIndex: "1" }}
+            src="/images/mainpage.png"
+            alt="mainpage"
+          />
+          <h2 className="img__title">Erzetich - a new preamp series</h2>
+          <h5 className="img__subtitle">
+            A fully new preamp station Erzitech - Bncillus
+          </h5>
+          <NavLink to="/about" className="img__btn">
+            <span style={{ display: "block", margin: "6px 10px 20px 60px" }}>
+              Learn more
+            </span>
+          </NavLink>
+        </div>
+
+        <CardItem />
+      </div>
       <div className="main-page">
         <div>
           <img src="/images/mainpage.png" alt="mainpage" />
@@ -117,7 +112,6 @@ function Home() {
           </div>
         </div>
       </div>
-
       <div
         className="main-subscribe"
         style={{ backgroundImage: "url('/images/subscribe-block-img.jpg')" }}
@@ -136,7 +130,6 @@ function Home() {
         </div>
       </div>
     </>
->>>>>>> a0c1dc822065dbca6130ba7fc361b339b2616df6
   );
 }
 export default Home;
